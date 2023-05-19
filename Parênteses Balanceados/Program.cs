8c0de43d55902d7a7a70eca88d9693e6ba92d387
Martarelli <renanmartarelli@gmail.com>
2023-05-19 10:28:52 -0300

    exercicio concluido

--- Parênteses Balanceados/Program.cs
+++ Parênteses Balanceados/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿string palavra = "(()()((())()((";
+
+int countParenteseEsq = 0;
+int countParenteseDir = 0;
+foreach(char parentese in palavra)
+{
+    if(parentese == '(')
+    {
+        countParenteseEsq++;
+    }
+    else if (parentese == ')')
+    {
+        countParenteseDir++;
+    }
+}
+return Math.Abs(countParenteseEsq-countParenteseDir);
