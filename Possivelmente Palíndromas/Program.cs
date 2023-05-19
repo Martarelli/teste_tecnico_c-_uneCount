de82516763d8d0dd4ebb43bd6815acf9715afc1b
Martarelli <renanmartarelli@gmail.com>
2023-05-19 10:50:06 -0300

    exercicio finalizado

--- Possivelmente Palíndromas/Program.cs
+++ Possivelmente Palíndromas/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string palavra = "temem";
+
+string str = palavra.Replace(" ", String.Empty);
+char[] chars = str.ToCharArray();
+Array.Reverse(chars);
+string strReverse = new string(chars);
+
+string strPotencial = palavra.Replace(" ", String.Empty);
+chars = strPotencial.ToCharArray();
+Array.Sort(chars);
+strPotencial = new string(chars);
+
+bool potencial = true;
+for (int i = 1; i < strPotencial.Length; i += 2) {
+    if(strPotencial[i] != strPotencial[i-1])
+    {
+        potencial = false;
+    }  
+}
+if(str == strReverse || palavra == "" || potencial){
+    return true;
+} else {
+    return false;
+}
