391d8e8e84f7eaab1eaf80c9d631d63f5d9e3cca
Martarelli <renanmartarelli@gmail.com>
2023-05-19 11:02:54 -0300

    exercicio finalizado

--- Problema da soma de dois números/Program.cs
+++ Problema da soma de dois números/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] numeros = {3,3};
+int objetivo = 6;
+
+List<int> index = new List<int>();
+for (int i = 0; i < numeros.Length; i++) {
+    System.Console.WriteLine(numeros[i]);
+    for (int j = i+1; j < numeros.Length; j++) {
+        System.Console.WriteLine(numeros[j]);
+        if (numeros[i] + numeros[j] == objetivo){
+            index.Add(i);
+            index.Add(j);
+        }
+    }      
+}
+return index.ToArray();
